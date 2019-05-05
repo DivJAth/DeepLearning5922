6fd6b7b94b5fab37d6efa07724da0712553f3a89
Mahesh Parab <maheshp2812@gmail.com>
2019-05-05 14:47:46 -0600

    Padded image preview. Tab panels working correctly now

--- WebServer/static/js/index.js
+++ WebServer/static/js/index.js
@@ -1,16 +1,13 @@
-$(function() {
-    $("#classification-input-files").change((event) => {
-        let input = event.target;
-        if(input.files) {
-            $("#classification-img-preview").html("");
-            for (let i = 0; i < input.files.length; i++) {
-                let reader = new FileReader();
-                reader.onload = function() {
-                    console.log('onload function called')
-                    $($.parseHTML('<img>')).attr('src', this.result).attr('width', '100px').appendTo("#classification-img-preview");
-                }
-                reader.readAsDataURL(input.files[i]);
+$("#classification-input-files").change((event) => {
+    let input = event.target;
+    if(input.files) {
+        $("#classification-img-preview").html("");
+        for (let i = 0; i < input.files.length; i++) {
+            let reader = new FileReader();
+            reader.onload = function() {
+                $($.parseHTML("<img>")).attr("src", this.result).attr("width", "100px").appendTo("#classification-img-preview");
             }
+            reader.readAsDataURL(input.files[i]);
         }
-    });
+    }
 });
