2c3ef2de97e13d29062fc3a66625fb32ca4c8734
Mahesh Parab <maheshp2812@gmail.com>
2019-05-05 13:10:41 -0600

    Front-end: Image selector displays image previews

--- WebServer/static/js/index.js
+++ WebServer/static/js/index.js
@@ -1,3 +1,16 @@
 $(function() {
-    
+    $("#classification-input-files").change((event) => {
+        let input = event.target;
+        if(input.files) {
+            $("#classification-img-preview").html("");
+            for (let i = 0; i < input.files.length; i++) {
+                let reader = new FileReader();
+                reader.onload = function() {
+                    console.log('onload function called')
+                    $($.parseHTML('<img>')).attr('src', this.result).attr('width', '100px').appendTo("#classification-img-preview");
+                }
+                reader.readAsDataURL(input.files[i]);
+            }
+        }
+    });
 });
