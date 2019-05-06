466e8d2259b673722d38d7ad01a104e29753f961
Mahesh Parab <maheshp2812@gmail.com>
2019-05-05 21:05:37 -0600

    Front-end can upload images to the backend. The server stores images in ./imgs

--- WebServer/app.py
+++ WebServer/app.py
@@ -1,8 +1,10 @@
 from flask import Flask, render_template, request
 from flask_cors import CORS
+from PIL import Image
 import requests
 import json
 import sys
+import os
 
 properties = json.load(open('properties.json', 'r'))
 ip = properties['ml_server']['ip']
@@ -11,6 +13,7 @@ port = properties['ml_server']['port']
 app = Flask(__name__)
 CORS(app)
 app.jinja_env.auto_reload = True
+app.config['IMG_UPLOAD_DIR'] = './imgs'
 
 @app.route('/')
 def get_index_page():
@@ -19,9 +22,15 @@ def get_index_page():
 # Example routing function for ML classification call
 @app.route('/ml/classification', methods=['POST'])
 def classification_req_handler():
-    # print(request.files)
-    response = requests.post('http://%s:%s/classification' % (ip, port), data=request.data)
+    files = request.files.to_dict(flat=False)['classification-input-files']
+    for file in files:
+        file.save(os.path.join(app.config['IMG_UPLOAD_DIR'], file.filename))
+    # response = requests.post('http://%s:%s/classification' % (ip, port), data=request.data)
     return app.response_class(
         response='response.text',
         status=200
     )
+
+@app.route('/ml/semantic_segmentation', methods=['POST'])
+def vgg16_semantic_segmentation_handler():
+    pass
